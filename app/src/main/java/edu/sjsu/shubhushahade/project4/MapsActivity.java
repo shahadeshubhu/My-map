1ea357cd95f461cc860a84adee8011275428211b
Shubhu Shahade <shubhankarmandar.shahade@sjsu.edu>
2021-05-05 08:40:06 +0530

    comment removed

--- app/src/main/java/edu/sjsu/shubhushahade/project4/MapsActivity.java
+++ app/src/main/java/edu/sjsu/shubhushahade/project4/MapsActivity.java
@@ -43,7 +43,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //uninstall.setOnClickListener(this::onClick);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
